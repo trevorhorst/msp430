d65c39feacbb0f9e4b5e6884a20b65578d863b25
Trevor Horst <trevor.horst@g3ti.net>
2023-06-27 21:53:11 -0400

    Working TX and RX.

--- projects/dev/src/project.c
+++ projects/dev/src/project.c
@@ -6,22 +6,13 @@
 
 #define LOOP_COUNTER    32000
 
-//-----------------------------------------------------------------------//
-//                Transmit and Receive interrupts                        //
-//-----------------------------------------------------------------------//
+#define TXLED BIT0
+#define RXLED BIT6
+#define TXD BIT2
+#define RXD BIT1
 
-#pragma vector = USCIAB0TX_VECTOR
-__interrupt void TransmitInterrupt(void)
-{
-  P1OUT  ^= BIT0;//light up P1.0 Led on Tx
-}
-
-#pragma vector = USCIAB0RX_VECTOR
-__interrupt void ReceiveInterrupt(void)
-{
-  P1OUT  ^= BIT6;     // light up P1.6 LED on RX
-  IFG2 &= ~UCA0RXIFG; // Clear RX flag
-}
+const char string[] = { "Hello World\r\n" };
+unsigned int i; //Counter
 
 int run(void)
 {
@@ -64,47 +55,111 @@ int run(void)
     //     for(uint16_t i = 0; i < LOOP_COUNTER; i++);
     // }
 
-    WDTCTL = WDTPW + WDTHOLD; // Stop the Watch dog
+    // WDTCTL = WDTPW + WDTHOLD; // Stop the Watch dog
 
-    //------------------- Configure the Clocks -------------------//
+    // //------------------- Configure the Clocks -------------------//
 
-    if (CALBC1_1MHZ==0xFF)   // If calibration constant erased
-       {
-          while(1);          // do not load, trap CPU!!
-       }
+    // if (CALBC1_1MHZ==0xFF)   // If calibration constant erased
+    //    {
+    //       while(1);          // do not load, trap CPU!!
+    //    }
 
-     DCOCTL  = 0;             // Select lowest DCOx and MODx settings
-     BCSCTL1 = CALBC1_1MHZ;   // Set range
-     DCOCTL  = CALDCO_1MHZ;   // Set DCO step + modulation
+    //  DCOCTL  = 0;             // Select lowest DCOx and MODx settings
+    //  BCSCTL1 = CALBC1_1MHZ;   // Set range
+    //  DCOCTL  = CALDCO_1MHZ;   // Set DCO step + modulation
 
-    //---------------- Configuring the LED's ----------------------//
+    // //---------------- Configuring the LED's ----------------------//
 
-     P1DIR  |=  BIT0 + BIT6;  // P1.0 and P1.6 output
-     P1OUT  &= ~BIT0 + BIT6;  // P1.0 and P1.6 = 0
+    //  P1DIR  |=  BIT0 + BIT6;  // P1.0 and P1.6 output
+    //  P1OUT  &= ~BIT0 + BIT6;  // P1.0 and P1.6 = 0
 
-    //--------- Setting the UART function for P1.1 & P1.2 --------//
+    // //--------- Setting the UART function for P1.1 & P1.2 --------//
 
-     P1SEL  |=  BIT1 + BIT2;  // P1.1 UCA0RXD input
-     P1SEL2 |=  BIT1 + BIT2;  // P1.2 UCA0TXD output
+    //  P1SEL  |=  BIT1 + BIT2;  // P1.1 UCA0RXD input
+    //  P1SEL2 |=  BIT1 + BIT2;  // P1.2 UCA0TXD output
 
-    //------------ Configuring the UART(USCI_A0) ----------------//
+    // //------------ Configuring the UART(USCI_A0) ----------------//
 
-     UCA0CTL1 |=  UCSSEL_2 + UCSWRST;  // USCI Clock = SMCLK,USCI_A0 disabled
-     UCA0BR0   =  104;                 // 104 From datasheet table-
-     UCA0BR1   =  0;                   // -selects baudrate =9600,clk = SMCLK
-     UCA0MCTL  =  UCBRS_1;             // Modulation value = 1 from datasheet
-     UCA0STAT |=  UCLISTEN;            // loop back mode enabled
-     UCA0CTL1 &= ~UCSWRST;             // Clear UCSWRST to enable USCI_A0
+    //  UCA0CTL1 |=  UCSSEL_2 + UCSWRST;  // USCI Clock = SMCLK,USCI_A0 disabled
+    //  UCA0BR0   =  104;                 // 104 From datasheet table-
+    //  UCA0BR1   =  0;                   // -selects baudrate =9600,clk = SMCLK
+    //  UCA0MCTL  =  UCBRS_1;             // Modulation value = 1 from datasheet
+    //  UCA0STAT |=  UCLISTEN;            // loop back mode enabled
+    //  UCA0CTL1 &= ~UCSWRST;             // Clear UCSWRST to enable USCI_A0
 
-    //---------------- Enabling the interrupts ------------------//
+    // //---------------- Enabling the interrupts ------------------//
 
-     IE2 |= UCA0TXIE;                  // Enable the Transmit interrupt
-     IE2 |= UCA0RXIE;                  // Enable the Receive  interrupt
-     _BIS_SR(GIE);                     // Enable the global interrupt
+    //  UC0IE |= UCA0TXIE;                  // Enable the Transmit interrupt
+    //  UC0IE |= UCA0RXIE;                  // Enable the Receive  interrupt
+    //  _BIS_SR(GIE);                     // Enable the global interrupt
 
-     UCA0TXBUF = 'A';                  // Transmit a byte
+    //  UCA0TXBUF = 'A';                  // Transmit a byte
 
-     _BIS_SR(LPM0_bits + GIE);         // Going to LPM0
+    //  // _BIS_SR(LPM0_bits + GIE);         // Going to LPM0
+
+    //  while(1) {
+    //      for(uint32_t i = 0; i < 32000; i++);
+    //  }
+
+     WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+     DCOCTL = 0; // Select lowest DCOx and MODx settings
+     BCSCTL1 = CALBC1_1MHZ; // Set DCO
+     DCOCTL = CALDCO_1MHZ;
+     P2DIR |= 0xFF; // All P2.x outputs
+     P2OUT &= 0x00; // All P2.x reset
+     P1SEL |= RXD + TXD ; // P1.1 = RXD, P1.2=TXD
+     P1SEL2 |= RXD + TXD ; // P1.1 = RXD, P1.2=TXD
+     P1DIR |= RXLED + TXLED;
+     P1OUT &= 0x00;
+     UCA0CTL1 |= UCSSEL_2; // SMCLK
+     UCA0BR0 = 52; // 1MHz 115200
+     UCA0BR1 = 0x00; // 1MHz 115200
+     UCA0MCTL = UCBRS2 + UCBRS0; // Modulation UCBRSx = 5
+     UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
+     UC0IE |= UCA0RXIE; // Enable USCI_A0 RX interrupt
+     __bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
+     while (1)
+     { }
 
     return 0;
+}
+
+//-----------------------------------------------------------------------//
+//                Transmit and Receive interrupts                        //
+//-----------------------------------------------------------------------//
+//
+// #pragma vector = USCIAB0TX_VECTOR
+// __interrupt void TransmitInterrupt(void)
+// {
+//   P1OUT  ^= BIT0;//light up P1.0 Led on Tx
+// }
+//
+// #pragma vector = USCIAB0RX_VECTOR
+// __interrupt void ReceiveInterrupt(void)
+// {
+//   P1OUT  ^= BIT6;     // light up P1.6 LED on RX
+//   IFG2 &= ~UCA0RXIFG; // Clear RX flag
+// }
+
+
+#pragma vector=USCIAB0TX_VECTOR
+__interrupt void USCI0TX_ISR(void)
+{
+   P1OUT |= TXLED;
+     UCA0TXBUF = string[i++]; // TX next character
+    if (i == sizeof string - 1) // TX over?
+       UC0IE &= ~UCA0TXIE; // Disable USCI_A0 TX interrupt
+    P1OUT &= ~TXLED; }
+
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR(void)
+{
+   P1OUT |= RXLED;
+    if (UCA0RXBUF == 'a') // 'a' received?
+    {
+       i = 0;
+       UC0IE |= UCA0TXIE; // Enable USCI_A0 TX interrupt
+      UCA0TXBUF = string[i++];
+    }
+    P1OUT &= ~RXLED;
 }
